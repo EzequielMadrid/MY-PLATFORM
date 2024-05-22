41bc1d2bb5baf5693800ae554c14031a49c09807
Ezequiel Madrid <madridezequiel299@gmail.com>
2024-05-22 19:07:40 -0300

    add new project

--- app/data.js
+++ app/data.js
@@ -1,21 +1,15 @@
 const projects = [
   {
-    image: "assets/img/tech-store.jpg",
-    type: "Tech Store",
-    appLink: "https://ezequielmadrid.github.io/tech-store/",
-    codeLink: "https://github.com/EzequielMadrid/tech-store",
+    image: "assets/img/resto.jpg",
+    type: "Exotic Resto",
+    appLink: "https://ezequielmadrid.github.io/exotic-restaurant/",
+    codeLink: "https://github.com/EzequielMadrid/exotic-restaurant",
   },
   {
     image: "assets/img/matrix-effect.jpg",
     type: "Matrix",
     appLink: "https://ezequielmadrid.github.io/matrix_effect/",
     codeLink: "https://github.com/EzequielMadrid/matrix_effect",
-  },
-  {
-    image: "assets/img/chat-1.6.jpg",
-    type: "Chat-1.6",
-    appLink: "https://ezequielmadrid.github.io/basic_chat/",
-    codeLink: "https://github.com/EzequielMadrid/basic_chat",
   },
   {
     image: "assets/img/hackers-uni.jpg",
@@ -40,5 +34,17 @@ const projects = [
     type: "Inventory",
     appLink: "https://ezequielmadrid.github.io/inventory/",
     codeLink: "https://github.com/EzequielMadrid/inventory",
+  },
+  {
+    image: "assets/img/tech-store.jpg",
+    type: "Tech Store",
+    appLink: "https://ezequielmadrid.github.io/tech-store/",
+    codeLink: "https://github.com/EzequielMadrid/tech-store",
+  },
+  {
+    image: "assets/img/chat-1.6.jpg",
+    type: "Chat-1.6",
+    appLink: "https://ezequielmadrid.github.io/basic_chat/",
+    codeLink: "https://github.com/EzequielMadrid/basic_chat",
   },
 ];
